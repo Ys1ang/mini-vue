d872f71666d1d3cae900f327d88a26bbab251ba0
杨泽平 <877377811@qq.com>
2023-02-06 11:11:19 +0800

    [fea] reactive 代理实现

---
+++ src/test/effec.spec.ts
@@ -0,0 +1,17 @@
+import {effect} from "../reactivity/effect";
+import {reactive} from "../reactivity/reactive";
+
+describe('effect', function () {
+    it('happy path', function () {
+        const user = reactive({age: 10})
+        let nextAge;
+        effect(() => {
+            nextAge = user.age + 1;
+        })
+        //
+        expect(nextAge).toBe(11);
+        // //update
+        // user.age++;
+        // expect(nextAge).toBe(12);
+    });
+});
